7cd9e1c03418988ee8b967b6a95d09fbcc44dbb7
Roman TIMAKOV <timakov_r@M11highway.pro>
2022-07-12 17:01:59 +0300

    Route

--- src/components/Card1/Card1.js
+++ src/components/Card1/Card1.js
@@ -63,6 +63,90 @@ function Card1(props) {
           <p className="table__paragraph">Дата окончания ФН: 01.01.2000</p>
           <p className="table__paragraph">Примерное кол-во дней до замены ФН: 500</p>
         </li>
+        <li className="table__cell_kkt">
+          <h3 className="table__heading_kkt">ККТ №0308600005025539</h3>
+          <p className="table__paragraph">Номер ФН 9960440300973139</p>
+          <p className="table__paragraph">Дата регистрации ФН: 01.01.2000</p>
+          <p className="table__paragraph">Дата окончания ФН: 01.01.2000</p>
+          <p className="table__paragraph">Примерное кол-во дней до замены ФН: 500</p>
+        </li>
+        <li className="table__cell_kkt">
+          <h3 className="table__heading_kkt">ККТ №0308600005025539</h3>
+          <p className="table__paragraph">Номер ФН 9960440300973139</p>
+          <p className="table__paragraph">Дата регистрации ФН: 01.01.2000</p>
+          <p className="table__paragraph">Дата окончания ФН: 01.01.2000</p>
+          <p className="table__paragraph">Примерное кол-во дней до замены ФН: 500</p>
+        </li>
+        <li className="table__cell_kkt">
+          <h3 className="table__heading_kkt">ККТ №0308600005025539</h3>
+          <p className="table__paragraph">Номер ФН 9960440300973139</p>
+          <p className="table__paragraph">Дата регистрации ФН: 01.01.2000</p>
+          <p className="table__paragraph">Дата окончания ФН: 01.01.2000</p>
+          <p className="table__paragraph">Примерное кол-во дней до замены ФН: 500</p>
+        </li>
+        <li className="table__cell_kkt">
+          <h3 className="table__heading_kkt">ККТ №0308600005025539</h3>
+          <p className="table__paragraph">Номер ФН 9960440300973139</p>
+          <p className="table__paragraph">Дата регистрации ФН: 01.01.2000</p>
+          <p className="table__paragraph">Дата окончания ФН: 01.01.2000</p>
+          <p className="table__paragraph">Примерное кол-во дней до замены ФН: 500</p>
+        </li>
+        <li className="table__cell_kkt">
+          <h3 className="table__heading_kkt">ККТ №0308600005025539</h3>
+          <p className="table__paragraph">Номер ФН 9960440300973139</p>
+          <p className="table__paragraph">Дата регистрации ФН: 01.01.2000</p>
+          <p className="table__paragraph">Дата окончания ФН: 01.01.2000</p>
+          <p className="table__paragraph">Примерное кол-во дней до замены ФН: 500</p>
+        </li>
+        <li className="table__cell_kkt">
+          <h3 className="table__heading_kkt">ККТ №0308600005025539</h3>
+          <p className="table__paragraph">Номер ФН 9960440300973139</p>
+          <p className="table__paragraph">Дата регистрации ФН: 01.01.2000</p>
+          <p className="table__paragraph">Дата окончания ФН: 01.01.2000</p>
+          <p className="table__paragraph">Примерное кол-во дней до замены ФН: 500</p>
+        </li>
+        <li className="table__cell_kkt">
+          <h3 className="table__heading_kkt">ККТ №0308600005025539</h3>
+          <p className="table__paragraph">Номер ФН 9960440300973139</p>
+          <p className="table__paragraph">Дата регистрации ФН: 01.01.2000</p>
+          <p className="table__paragraph">Дата окончания ФН: 01.01.2000</p>
+          <p className="table__paragraph">Примерное кол-во дней до замены ФН: 500</p>
+        </li>
+        <li className="table__cell_kkt">
+          <h3 className="table__heading_kkt">ККТ №0308600005025539</h3>
+          <p className="table__paragraph">Номер ФН 9960440300973139</p>
+          <p className="table__paragraph">Дата регистрации ФН: 01.01.2000</p>
+          <p className="table__paragraph">Дата окончания ФН: 01.01.2000</p>
+          <p className="table__paragraph">Примерное кол-во дней до замены ФН: 500</p>
+        </li>
+        <li className="table__cell_kkt">
+          <h3 className="table__heading_kkt">ККТ №0308600005025539</h3>
+          <p className="table__paragraph">Номер ФН 9960440300973139</p>
+          <p className="table__paragraph">Дата регистрации ФН: 01.01.2000</p>
+          <p className="table__paragraph">Дата окончания ФН: 01.01.2000</p>
+          <p className="table__paragraph">Примерное кол-во дней до замены ФН: 500</p>
+        </li>
+        <li className="table__cell_kkt">
+          <h3 className="table__heading_kkt">ККТ №0308600005025539</h3>
+          <p className="table__paragraph">Номер ФН 9960440300973139</p>
+          <p className="table__paragraph">Дата регистрации ФН: 01.01.2000</p>
+          <p className="table__paragraph">Дата окончания ФН: 01.01.2000</p>
+          <p className="table__paragraph">Примерное кол-во дней до замены ФН: 500</p>
+        </li>
+        <li className="table__cell_kkt">
+          <h3 className="table__heading_kkt">ККТ №0308600005025539</h3>
+          <p className="table__paragraph">Номер ФН 9960440300973139</p>
+          <p className="table__paragraph">Дата регистрации ФН: 01.01.2000</p>
+          <p className="table__paragraph">Дата окончания ФН: 01.01.2000</p>
+          <p className="table__paragraph">Примерное кол-во дней до замены ФН: 500</p>
+        </li>
+        <li className="table__cell_kkt">
+          <h3 className="table__heading_kkt">ККТ №0308600005025539</h3>
+          <p className="table__paragraph">Номер ФН 9960440300973139</p>
+          <p className="table__paragraph">Дата регистрации ФН: 01.01.2000</p>
+          <p className="table__paragraph">Дата окончания ФН: 01.01.2000</p>
+          <p className="table__paragraph">Примерное кол-во дней до замены ФН: 500</p>
+        </li>
       </ul>
     </div>
   )
