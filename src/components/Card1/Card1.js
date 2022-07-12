c0db5e224f768e5ce10d9c3130c91bcefc75e616
Roman TIMAKOV <timakov_r@M11highway.pro>
2022-07-12 16:49:38 +0300

    4

--- src/components/Card1/Card1.js
+++ src/components/Card1/Card1.js
@@ -35,12 +35,12 @@ function Card1(props) {
           <p className="table__paragraph">Дата окончания ФН: 01.01.2000</p>
           <p className="table__paragraph">Примерное кол-во дней до замены ФН: 500</p>
         </li>
-        <li className="table__cell_kkt">
+        <li className="table__cell_kkt_red">
           <h3 className="table__heading_kkt">ККТ №0308600005025539</h3>
           <p className="table__paragraph">Номер ФН 9960440300973139</p>
           <p className="table__paragraph">Дата регистрации ФН: 01.01.2000</p>
           <p className="table__paragraph">Дата окончания ФН: 01.01.2000</p>
-          <p className="table__paragraph">Примерное кол-во дней до замены ФН: 500</p>
+          <p className="table__paragraph">Примерное кол-во дней до замены ФН: 2</p>
         </li>
         <li className="table__cell_kkt">
           <h3 className="table__heading_kkt">ККТ №0308600005025539</h3>
@@ -49,12 +49,12 @@ function Card1(props) {
           <p className="table__paragraph">Дата окончания ФН: 01.01.2000</p>
           <p className="table__paragraph">Примерное кол-во дней до замены ФН: 500</p>
         </li>
-        <li className="table__cell_kkt">
+        <li className="table__cell_kkt_yellow">
           <h3 className="table__heading_kkt">ККТ №0308600005025539</h3>
           <p className="table__paragraph">Номер ФН 9960440300973139</p>
           <p className="table__paragraph">Дата регистрации ФН: 01.01.2000</p>
           <p className="table__paragraph">Дата окончания ФН: 01.01.2000</p>
-          <p className="table__paragraph">Примерное кол-во дней до замены ФН: 500</p>
+          <p className="table__paragraph">Примерное кол-во дней до замены ФН: 10</p>
         </li>
         <li className="table__cell_kkt">
           <h3 className="table__heading_kkt">ККТ №0308600005025539</h3>
